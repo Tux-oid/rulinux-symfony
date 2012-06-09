aa72838b4064df9f9351da490f74d520230a192f
Peter Vasilevsky <tuxoiduser@gmail.com>
2012-06-09 16:38:24 +0400

    small fix

--- src/RL/ForumBundle/Entity/ThreadRepository.php
+++ src/RL/ForumBundle/Entity/ThreadRepository.php
@@ -17,17 +17,12 @@ class ThreadRepository extends EntityRepository
 			->select('t, m')
 			->from('RL\ForumBundle\Entity\Thread', 't')
 			->innerJoin('t.messages', 'm')
-			->where('m.id = ('.$em->createQueryBuilder()->select('msg.id')
-				->from('RL\ForumBundle\Entity\Message', 'msg')
-				->where('msg.thread = t.id')
-				->orderBy('msg.id')
-				->setMaxResults('1')->getDQL().')')
+			->where('m.id = (SELECT MIN(msg.id) AS msg_id FROM RL\ForumBundle\Entity\Message AS msg WHERE msg.thread = t.id)')
 			->andWhere('t.subsection = (SELECT s.id FROM RL\ForumBundle\Entity\Subsection s WHERE s.rewrite = ?1)')
 			->setParameter(1, $subsection)
 			->orderBy('t.id')
 			->setFirstResult($offset)
 			->setMaxResults($limit)
-			
 			->getQuery();
 		$threads = $q->getResult();
 		return $threads;
