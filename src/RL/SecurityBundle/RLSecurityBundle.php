c87ffdca1a2830e47d33e47b49d2f6c9c7130569
molchanoviv <qwe123QWE>
2012-10-15 15:56:29 +0400

    migration is continued

--- src/RL/SecurityBundle/RLSecurityBundle.php
+++ src/RL/SecurityBundle/RLSecurityBundle.php
@@ -1,11 +1,21 @@
 <?php
 /**
- *@author Ax-xa-xa 
+ * @author Ax-xa-xa
+ * @author Tux-oid
  */
 namespace RL\SecurityBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use RL\SecurityBundle\Security\Factory\AnonymousSecurityFactory;
 
 class RLSecurityBundle extends Bundle
 {
+	public function build(ContainerBuilder $container)
+	{
+		parent::build($container);
+
+		$extension = $container->getExtension('security');
+		$extension->addSecurityListenerFactory(new AnonymousSecurityFactory());
+	}
 }
